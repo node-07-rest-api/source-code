a305165918179eb19540c0a63bd58e0ccd100c73
course-mentor <67462207+vlad-khrychov@users.noreply.github.com>
2023-08-22 15:16:27 +0300

    Add products.json and send respons from it

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const http = require("http");
 const url = require("url");
+const products = require("./products.json");
 
 const app = http.createServer((req, res) => {
   const { pathname } = url.parse(req.url);
-  if (pathname==='/products') {
-    res.writeHead(200)
-    res.end('products')
+  if (pathname === "/products") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(products));
   } else {
-    res.writeHead(404)
-    res.end(`The route "${pathname}" does not exist`)
+    res.writeHead(404);
+    res.end(`The route "${pathname}" does not exist`);
   }
-
 });
 
 const PORT = 3000;
