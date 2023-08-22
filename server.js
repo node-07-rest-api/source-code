9ec8a10a1514a7a623c63065fce71ca48a65d0c1
course-mentor <67462207+vlad-khrychov@users.noreply.github.com>
2023-08-22 15:02:45 +0300

    Implement GET products and 404 error

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const http = require("http");
+const url = require("url");
 
-const app = http.createServer(() => {
-  console.log("Hello from server");
+const app = http.createServer((req, res) => {
+  const { pathname } = url.parse(req.url);
+  if (pathname==='/products') {
+    res.writeHead(200)
+    res.end('products')
+  } else {
+    res.writeHead(404)
+    res.end(`The route "${pathname}" does not exist`)
+  }
+
 });
 
 const PORT = 3000;
