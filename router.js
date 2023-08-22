5418ae015e0b943c6668e327b794f5e5e067a8ce
course-mentor <67462207+vlad-khrychov@users.noreply.github.com>
2023-08-22 18:04:28 +0300

    Create router placeholders for product

--- router.js
+++ router.js
@@ -7,6 +7,14 @@ const router = (req, res) => {
     getProducts(res);
   } else if (pathname === "/products" && req.method === "POST") {
     addProduct(req, res);
+  } else if (pathname.match(/^\/products\/([a-zA-Z0-9-]+)$/)) {
+    if (req.method === "GET") {
+      res.end("product GET");
+    } else if (req.method === "DELETE") {
+      res.end("product DELETE");
+    } else if (req.method === "PATCH") {
+      res.end("product PATCH");
+    }
   } else {
     res.writeHead(404);
     res.end(`The route "${pathname}" does not exist`);
